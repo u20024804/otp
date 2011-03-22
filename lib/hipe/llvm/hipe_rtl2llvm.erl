8cbe9889b356227f20bfea3156161c10cde292e2
Yiannis Tsiouris <yiannis.tsiouris@gmail.com>
2011-03-22 19:40:43 +0200

    hipe_rtl2llvm:translate/1: print RTL to file

--- lib/hipe/llvm/hipe_rtl2llvm.erl
+++ lib/hipe/llvm/hipe_rtl2llvm.erl
@@ -8,10 +8,11 @@
 
 translate(RTL) ->
     io:format("Geia sou llvm!~n"),
-    {ok, File_llvm} = file:open("out.ll", [read, write]),
-    %%{ok, File_rtl} = file:open("out.rtl", [write, raw]),
-    %%hipe_rtl:pp(File_rtl, RTL),
-    %%file:close(File_rtl).
+    {ok, File_rtl} = file:open("out.rtl", [append]),
+    hipe_rtl:pp(File_rtl, RTL),
+    file:close(File_rtl),
+
+    {ok, File_llvm} = file:open("out.ll", [append]),
     Data = hipe_rtl:rtl_data(RTL),
     Code = hipe_rtl:rtl_code(RTL),
     translate_instrs(File_llvm, Code),
