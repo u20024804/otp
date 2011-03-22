1fd07b0a393d62f2e390d457c16a702b80e3a6cb
Yiannis Tsiouris <yiannis@mosby.yatsi.org>
2011-03-22 15:51:13 +0200

    First commit to llvm/. Added hipe_rtl2llvm.erl (foo-file) to work with main/hipe_main.erl.

---
+++ lib/hipe/llvm/hipe_rtl2llvm.erl
@@ -0,0 +1,10 @@
+%% -*- erlang-indent-level: 2 -*-
+
+-module(hipe_rtl2llvm).
+-author("Chris Stavrakakis, Yiannis Tsiouris").
+-include("../rtl/hipe_rtl.hrl").
+
+-export([translate/0]).
+
+translate() ->
+  io:format("Geia sou llvm!~n").
