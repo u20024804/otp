9456a10cd41596737ea842ac941bc9727570cf21
Chris Stavrakakis <stavr.chris@gmail.com>
2011-04-25 16:03:55 +0300

    Grouped printing of binary and bitwise operations

--- lib/hipe/llvm/hipe_llvm.hrl
+++ lib/hipe/llvm/hipe_llvm.hrl
@@ -14,25 +14,25 @@
 -record(llvm_br_cond, {'cond', true_label, false_label}).
 %% Binary Operations
 -record(llvm_operation, {dst, op, type, src1, src2, options=[]}).
--record(llvm_add, {dst, type, src1, src2, options=[]}).
--record(llvm_fadd, {dst, type, src1, src2, options=[]}).
--record(llvm_sub, {dst, type, src1, src2, options=[]}).
--record(llvm_fsub, {dst, type, src1, src2, options=[]}).
--record(llvm_mul, {dst, type, src1, src2, options=[]}).
--record(llvm_fmul, {dst, type, src1, src2, options=[]}).
--record(llvm_udiv, {dst, type, src1, src2, options=[]}).
--record(llvm_sdiv, {dst, type, src1, src2, options=[]}).
--record(llvm_fdiv, {dst, type, src1, src2, options=[]}).
--record(llvm_urem, {dst, type, src1, src2, options=[]}).
--record(llvm_srem, {dst, type, src1, src2, options=[]}).
--record(llvm_frem, {dst, type, src1, src2, options=[]}).
-%% Bitwise Binary Operations
--record(llvm_shl, {dst, type, src1, src2, options=[]}).
--record(llvm_lshr, {dst, type, src1, src2, options=[]}).
--record(llvm_ashr ,{dst, type, src1, src2, options=[]}).
--record(llvm_and,{dst, type, src1, src2}).
--record(llvm_or, {dst, type, src1, src2}).
--record(llvm_xor, {dst, type, src1, src2}).
+%-record(llvm_add, {dst, type, src1, src2, options=[]}).
+%-record(llvm_fadd, {dst, type, src1, src2, options=[]}).
+%-record(llvm_sub, {dst, type, src1, src2, options=[]}).
+%-record(llvm_fsub, {dst, type, src1, src2, options=[]}).
+%-record(llvm_mul, {dst, type, src1, src2, options=[]}).
+%-record(llvm_fmul, {dst, type, src1, src2, options=[]}).
+%-record(llvm_udiv, {dst, type, src1, src2, options=[]}).
+%-record(llvm_sdiv, {dst, type, src1, src2, options=[]}).
+%-record(llvm_fdiv, {dst, type, src1, src2, options=[]}).
+%-record(llvm_urem, {dst, type, src1, src2, options=[]}).
+%-record(llvm_srem, {dst, type, src1, src2, options=[]}).
+%-record(llvm_frem, {dst, type, src1, src2, options=[]}).
+%%% Bitwise Binary Operations
+%-record(llvm_shl, {dst, type, src1, src2, options=[]}).
+%-record(llvm_lshr, {dst, type, src1, src2, options=[]}).
+%-record(llvm_ashr ,{dst, type, src1, src2, options=[]}).
+%-record(llvm_and,{dst, type, src1, src2}).
+%-record(llvm_or, {dst, type, src1, src2}).
+%-record(llvm_xor, {dst, type, src1, src2}).
 %% Aggregate Operations
 -record(llvm_extractvalue, {dst, type, val, idx, idxs=[]}).
 %% Memory Access And Addressing Operations
