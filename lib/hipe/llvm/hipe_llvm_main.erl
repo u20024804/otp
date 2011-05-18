3eb61899cda0444ac36d1ddc0256924659846ba3
Yiannis Tsiouris <yiannis.tsiouris@gmail.com>
2011-05-18 18:23:14 +0300

    Corrections in triggering LLVM toolchain

--- lib/hipe/llvm/hipe_llvm_main.erl
+++ lib/hipe/llvm/hipe_llvm_main.erl
@@ -6,7 +6,7 @@
 -export([rtl_to_native/2]).
 
 
-rtl_to_native(RTL, Options) ->
+rtl_to_native(RTL, _Options) ->
   %% Get LLVM Instruction List
   {LLVMCode, RefDict} = hipe_rtl2llvm:translate(RTL),
   %% Write LLVM Assembly to intermediate file
@@ -15,9 +15,10 @@ rtl_to_native(RTL, Options) ->
   {ok, File_llvm} = file:open(atom_to_list(Fun_Name) ++ ".ll", [write]),
   hipe_llvm:pp_ins_list(File_llvm, LLVMCode),
   %% Invoke LLVM compiler tool to produce an object file
-  {ok, Object_File} = llvmc(atom_to_list(Fun_Name)),
+  Filename = atom_to_list(Fun_Name),
+  Object_filename = compile_with_llvm(Filename),
   %% Extract information from object file
-  ObjBin = elf64_format:open_object_file(Object_File),
+  ObjBin = elf64_format:open_object_file(Object_filename),
   %% Get relocation info and write to file for loader
   Relocs = elf64_format:get_call_list(ObjBin),
   Relocs1 = lists:map(fun({A,B}) -> {map_funs(A, RefDict), B} end, Relocs),
@@ -38,20 +39,65 @@ rtl_to_native(RTL, Options) ->
   {BinCode, FinalRelocs}.
 
 
-llvmc(Fun_Name) -> 
+fix_opts(Opts) ->
+  lists:foldl(fun(X, Acc) -> Acc++" "++X end, "", Opts).
+
+
+%% Compile with LLVM tools
+compile_with_llvm(Fun_Name) ->
+  Opt_filename = opt(Fun_Name),
+  llc(Opt_filename, Fun_Name),  %XXX: Both names needed. FIX THIS SHIT
+  llvmc(Fun_Name).
+
+  
+%% OPT wrapper (.ll -> .ll)
+opt(Fun_Name) ->
+  Options = ["-O3"],
+  opt(Fun_Name, Options).
+
+opt(Fun_Name, Opts) ->
+  Llvm_file = Fun_Name ++ ".ll",
+  Opt_llvm_filename = Fun_Name ++ "_42_", %New (optimized) file
+  Opt_llvm_file = Opt_llvm_filename ++ ".ll",
+  Command = "opt " ++ fix_opts(Opts) ++ " -S" ++ " -o " ++ Opt_llvm_file ++ " "
+    ++ Llvm_file,
+  case os:cmd(Command) of
+    [] -> ok;
+    Error -> exit({?MODULE, opt, Error})
+  end,
+  Opt_llvm_filename.
+
+
+%% LLC wrapper (.ll -> .s)
+llc(Opt_filename, Fun_Name) ->
   Options = ["-O3", "-code-model=medium"],
+  llc(Opt_filename, Fun_Name, Options).
+
+llc(Opt_filename, Fun_Name, Opts) ->
+  Llvm_file = Opt_filename ++ ".ll",
+  Asm_file = Fun_Name ++ ".s",
+  Command = "llc " ++ fix_opts(Opts) ++ " " ++ Llvm_file ++ " -o " ++ Asm_file,
+  case os:cmd(Command) of
+    [] -> ok;
+    Error -> exit({?MODULE, llvmc, Error})
+  end.
+
+
+%% LLVMC wrapper (.s -> .o)
+llvmc(Fun_Name) -> 
+  Options = [],
   llvmc(Fun_Name, Options).
 
-llvmc(Fun_Name, Options) ->
-  Llvm_File = Fun_Name++".ll",
-  Object_File = Fun_Name++".o",
-  Options2 = lists:foldl(fun(X, Acc) -> Acc++" "++X end, "", Options),
-  Command = "llvmc "++Options2++" -o "++Object_File++" -c "++Llvm_File,
+llvmc(Fun_Name, Opts) ->
+  Asm_File = Fun_Name++".s",
+  Object_filename = Fun_Name ++ ".o",
+  Command = "llvmc " ++ fix_opts(Opts) ++ " -c " ++ Asm_File ++ " -o " ++
+    Object_filename,
   case os:cmd(Command) of
     [] -> ok;
     Error -> exit({?MODULE, llvmc, Error})
   end,
-  {ok, Object_File}.
+  Object_filename.
 
 
 map_funs(Name, Dict) ->
