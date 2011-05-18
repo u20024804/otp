4811ccb189e7298a90842d991b4925a9fccce19c
Chris Stavrakakis <hydralisk.r@gmail.com>
2011-05-18 18:24:59 +0300

    Some more tests

---
+++ lib/hipe/llvm/test/call_test.erl
@@ -0,0 +1,19 @@
+-module(call_test).
+-compile(export_all).
+
+factorial(0) -> 0;
+factorial(1) -> 0;
+factorial(X) ->
+  X*factorial(X-1).
+
+sum(1) -> 1;
+sum(X) -> X+sum(X-1).
+
+
+plusplus(X) -> 
+  A = math_test:inc(X),
+  math_test:inc(A).
+
+fib(0) -> 0;
+fib(1) -> 1;
+fib(X) -> fib(X-1)+fib(X-2).
