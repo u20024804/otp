35a6c388e7106f5003632250827fa55806c45fe8
Chris Stavrakakis <hydralisk.r@gmail.com>
2011-09-21 17:37:15 +0300

    Changed LLVM-specific BIFs

--- erts/emulator/hipe/hipe_bif2.c
+++ erts/emulator/hipe/hipe_bif2.c
@@ -168,14 +168,10 @@ BIF_RETTYPE hipe_bifs_show_message_area_0(BIF_ALIST_0)
 }
 
 /* Stub-BIF for LLVM:
- * Reloads BP, SP (in llvm unwind label) and realigns stack
- * in case of more than 4 arguments) */
+ * Reloads BP, SP (in llvm unwind label) */
 
-BIF_RETTYPE hipe_bifs_llvm_fixstack_1(BIF_ALIST_1)
+BIF_RETTYPE hipe_bifs_llvm_fix_pinned_regs_0(BIF_ALIST_0)
 {
-    Sint n = signed_val(BIF_ARG_1);
-    if (n > 0) 
-        asm volatile("subq %0, %%rsp\t # llvm_fixstack\n" : : "r"(n) : "%rsp");
     BIF_RET(am_ok);
 }
 
